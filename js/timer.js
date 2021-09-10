d4f1f13ec3d4d6479991690e39d34160c6dcb7c1
Adam <adamdryit@gmail.com>
2021-09-10 09:16:00 +0100

    Complete

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-const countDownDate = new Date("Sep 13, 2022 10:00:00").getTime()
+const countDownDate = new Date("Sep 11, 2022 00:00:00").getTime()
 
 // Update the count down every 1 second
 setInterval(function() {
